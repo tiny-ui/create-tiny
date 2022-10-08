27de724ebf3ccba96cdb8cab2677a6d25bd1e3d1
HarlonWang <81813780@qq.com>
2022-10-08 22:11:02 +0800

    init

--- index.ts
+++ index.ts
@@ -1,9 +1,27 @@
 #!/usr/bin/env node
-
-console.log('before')
+import prompts from 'prompts'
+import { red, green, bold } from 'kolorist'
 
 async function init() {
-    console.log('init')
+    console.log('Tiny.js')
+
+    const cwd = process.cwd()
+    try {
+        await prompts([
+            {
+                name: 'projectName',
+                type: 'text',
+                message: 'Project name:',
+                initial: 'defaultProjectName'
+            }
+        ], {
+            onCancel: () => {
+                throw new Error(red('✖') + ' Operation cancelled')
+            }})
+    } catch (e) {
+        console.log(e.message)
+        process.exit(1)
+    }
 }
 
 init().catch((e) => {
